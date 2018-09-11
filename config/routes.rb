bc241bd8ce494d05a7580a46dc786b39afb1a53c
vabesa <vabesa@uc.cl>
2018-09-11 11:39:29 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :homes
   get 'home/index'
 
-  root 'home#index'
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
