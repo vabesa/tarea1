025531aa19db03eb6c93093403796a29a5b49f7d
vabesa <vabesa@uc.cl>
2018-09-11 11:49:44 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
   resources :homes
-  get 'home/index'
+  get 'posts/index', to 'posts#index' 
 
   root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
